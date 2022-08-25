daef2030b6b887e94bc1b52d20ae277bfbc9ba53
Nayan Smuek <sanjanksen4@gmail.com>
2022-08-25 15:24:02 -0700

    honselty this class is so fun im learning so much and im so glad i took it :)))))))))))))))))))))))))))))))))))))))))))))

---
+++ August 25/ascy.java
@@ -0,0 +1,19 @@
+class ascy {
+    public static void main(String[] args) {
+      //System.out.println(" \"Hello\" + world ");
+
+      //System.out.println("Here I am \nnext line now");
+
+      //System.out.println("\\here\\");
+
+      System.out.println("All \"good\" men should come to the aid of their country.");
+      System.out.println("Hello \nhello again");
+      System.out.println("A backslash looks like this \\, …right?");
+
+      System.out.println(1+1+"8-2" + (8-2)+1+1);
+
+
+
+
+    }
+  }
