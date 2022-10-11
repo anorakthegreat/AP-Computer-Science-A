0bfcb741c7714b4643495b7f412b49fd5ebb6f1c
Nayan Smuek <sanjanksen4@gmail.com>
2022-10-11 14:30:55 -0700

    tai said YES omg the weddings gonna be great

---
+++ October 11/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String str1 = "AP CSA";
+        String str2 = "AP CSA"; //str1 and str2 have same adress or reference
+        System.out.println(str1==str2);
+        System.out.println(str1.equals(str2));
+
+
+        
+  }
+}
