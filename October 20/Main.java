07f75f98eaa43101bb901e29ce2c6dc1e083f9dd
Nayan Smuek <sanjanksen4@gmail.com>
2022-10-20 15:16:44 -0700

    agreeance

---
+++ October 20/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+      int x = 19;
+      int y = 2000;
+      boolean z = (x > 15 && x < 18) || (x > 10 || y < 20);
+      boolean w = (x < 15 && x > 18) || (x < 10 && y > 20);
+
+      System.out.println(z);
+      System.out.println(w);
+
+
+
+        
+  }
+}
